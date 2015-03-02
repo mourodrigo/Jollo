eab3fac6dfc5c21df08b7a411f4aa2c9989cd550
mourodrigo <rodrigobt20@gmail.com>
2015-03-02 15:07:16 -0300

    argsint

--- Jollo/main.cpp
+++ Jollo/main.cpp
@@ -7,9 +7,42 @@
 //
 
 #include <iostream>
+using namespace std;
+
+#define maior(a, b, c) ( ((a) < (b)) && ((b) < (c)) )
+//,atoi(    [5]),atoi(argv[6])
+void retornaVetorOrdenado(int a, int b, int c){
+    printf("%d %d %d", a,b,c);
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    if (argc==6) {
+        int argsint[]={atoi(argv[1]),atoi(argv[2]), atoi(argv[3])};
+        if (maior(argsint[0], argsint[1], argsint[2])) {
+            retornaVetorOrdenado(argsint[0], argsint[1], argsint[2]);
+        }else if(maior(argsint[0], argsint[2], argsint[1])) {
+            retornaVetorOrdenado(argsint[0], argsint[2], argsint[1]);
+            
+        }else if(maior(argsint[1], argsint[0], argsint[2])){
+            retornaVetorOrdenado(argsint[1], argsint[0], argsint[2]);
+            
+        }else if(maior(argsint[1], argsint[2], argsint[0])){
+            retornaVetorOrdenado(argsint[1], argsint[2], argsint[0]);
+            
+        }else if(maior(argsint[2], argsint[0], argsint[1])){
+            retornaVetorOrdenado(argsint[2], argsint[0], argsint[1]);
+            
+        }else if(maior(argsint[2], argsint[1], argsint[0])){
+            retornaVetorOrdenado(argsint[2], argsint[1], argsint[0]);
+            
+        }
+        
+    }else{
+        cout << "Faltam valores a serem informados";
+    }
+    
+    
+    
     return 0;
 }
